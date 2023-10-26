27946a60e174c69e6338c7e9cb04104bfb20cb46
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2023-10-26 18:39:58 -0500

    from begining untill create connection string

---
+++ ADODemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ADODemo.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADODemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IConfiguration _configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            string connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connectionString;
+        }
+    }
+}
