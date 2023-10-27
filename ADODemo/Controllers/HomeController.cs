f4943c559a8f11d3e95c9809e70d18fa68a2dcf0
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2023-10-26 20:12:22 -0500

    refactoring and do both connectd and disconnected

--- ADODemo/Controllers/HomeController.cs
+++ ADODemo/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,40 +25,62 @@ namespace ADODemo.Controllers
         public IEnumerable<Inventory> Index()
         {
             List<Inventory> list = new List<Inventory>();
-            // connection
+            DataTable dataTable = new DataTable();
+
+            #region 1- Connection
             string connectionString = _configuration["ConnectionStrings:DefaultConnection"];
             //SqlConnection connection = new SqlConnection(connectionString);
 
-
+            //
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
             builder.Encrypt = true;
             builder.TrustServerCertificate = true;
+
             SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            #endregion
 
-            //command
-            string sql = "Select * From dbo.Inventory";
+            #region 2- Command
+            //string sql = "Select * From dbo.Inventory";
+            string sql = "ReadInventory";
             SqlCommand command = new SqlCommand(sql, connection);
+            command.CommandType = CommandType.StoredProcedure; // default is text
+            #endregion
 
-            // execute command
-            connection.Open();
-            using (SqlDataReader dataReader = command.ExecuteReader())
+            #region 3- execute Command
+            #region Connected Mode using DataReader
+            //connection.Open();
+            //using (SqlDataReader dataReader = command.ExecuteReader())
+            //{
+            //    // Loop over the results
+            //    while (dataReader.Read())
+            //    {
+            //        list.Add(new Inventory
+            //        {
+            //            Id = Convert.ToInt32(dataReader["Id"]),
+            //            Name = Convert.ToString(dataReader["Name"]),
+            //            Price = Convert.ToDecimal(dataReader["Price"]),
+            //            Quantity = Convert.ToInt32(dataReader["Quantity"]),
+            //            AddedOn = Convert.ToDateTime(dataReader["AddedOn"])
+            //        }
+            //        );
+            //    }
+            //} 
+            //connection.Close();
+            #endregion
+
+            #region DisConnected Mode using DataAdapter
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            dataAdapter.Fill(dataTable);
+            list = dataTable.AsEnumerable().Select(row => new Inventory
             {
-                // Loop over the results
-                while (dataReader.Read())
-                {
-                    list.Add(new Inventory
-                    {
-                        Id = Convert.ToInt32(dataReader["Id"]),
-                        Name = Convert.ToString(dataReader["Name"]),
-                        Price = Convert.ToDecimal(dataReader["Price"]),
-                        Quantity = Convert.ToInt32(dataReader["Quantity"]),
-                        AddedOn = Convert.ToDateTime(dataReader["AddedOn"])
-                    }
-                    );
-                }
-            }
-            connection.Close();
-
+                Id = row.Field<int>("ID"),
+                Name = row.Field<string>("Name"),
+                Price = row.Field<decimal>("Price"),
+                Quantity = row.Field<int>("Quantity"),
+                AddedOn = row.Field<DateTime>("AddedOn"),
+            }).ToList();
+            #endregion 
+            #endregion
 
             return list;
         }
